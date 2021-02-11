7e1775f1067e1651669ec310d2e2259f41da5734
manikanta105 <78895847+manikanta105@users.noreply.github.com>
2021-02-11 15:00:11 +0530

    hiii

---
+++ abc.py
@@ -0,0 +1,10 @@
+def a(L,inp):
+    #inp=input()
+    if(inp in L):
+        print("yeah! we are in workshop...........just chill and learn more")
+    else:
+        print("Oh no! try to go to Lab 9 immediately....")
+
+L=["workshop on git and github by APSSDC","abcdef.."]
+inp=input()
+a(L,inp)
